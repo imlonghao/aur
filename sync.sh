bf82758486d81344cb7882d9a151df831e5e87d4
imlonghao <git@imlonghao.com>
2020-02-04 22:17:15 +0800

    CI: fix working directory
    gosec: New version v2.2.0

--- sync.sh
+++ sync.sh
@@ -3,13 +3,14 @@ set -e
 
 REPOS=$(git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g' | sed 's/\/$//g' | fgrep -v .github/workflows)
 COMMIT=$(git log -1 --pretty=%B)
+DIRECTORY=$(pwd)
 
 for REPO in $REPOS
 do
     cd /tmp
     git clone ssh://aur@aur.archlinux.org/$REPO.git
     cd $REPO
-    cp -r /home/runner/work/actions/actions/$REPO/* .
+    cp -r $DIRECTORY/$REPO/* .
     git add .
     git commit -m $(echo $COMMIT | grep $REPO | awk -F "$REPO: " '{print $2}')
     git push

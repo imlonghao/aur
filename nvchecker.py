23738814137f06e21b2af7e8d8de37eae9345451
imlonghao <git@imlonghao.com>
2020-02-17 23:41:12 +0800

    Add nvchecker for package update
    [skip ci]

---
+++ nvchecker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+from github import Github
+
+g = Github(os.getenv('GITHUB_TOKEN'))
+repo = g.get_repo('imlonghao/aur')
+open_issues = repo.get_issues(state='open', labels=['out-of-date'])
+
+for line in sys.stdin:
+    this = json.loads(line)
+    print(this)
+    if this['event'] != 'updated':
+        continue
+    opened = False
+    for issue in open_issues:
+        if this['name'] in issue.title:
+            opened = True
+            break
+    if not opened:
+        repo.create_issue(title='%s: updated from %s to %s' % (this['name'], this['old_version'], this['version']),
+                          labels=['out-of-date'],
+                          assignee='imlonghao')
---
+++ nvchecker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -f /tmp/oldver /tmp/newver
+
+for package in */
+do
+    source $package/PKGBUILD
+    echo "$pkgname $pkgver" >> /tmp/oldver
+done
+
+nvchecker --logger=json nvchecker.ini | python nvchecker.py

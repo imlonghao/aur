e1eb5a8966dbb89ab3e4788ea44c53a0961b5a2b
imlonghao <git@imlonghao.com>
2020-07-01 22:37:01 +0800

    Add GitLab CI

--- nvchecker.py
+++ nvchecker.py
@@ -3,24 +3,31 @@
 import os
 import sys
 import json
-from github import Github
+from gitlab import Gitlab
 
-g = Github(os.getenv('GITHUB_TOKEN'))
-repo = g.get_repo('imlonghao/aur')
-open_issues = repo.get_issues(state='open', labels=['out-of-date'])
+g = Gitlab('https://gitlab.com', job_token=os.environ['CI_JOB_TOKEN'])
+repo = g.projects.get(19694297)
+open_issues = repo.issues.list(state='opened', labels=['out-of-date'])
 
 for line in sys.stdin:
     this = json.loads(line)
     print(this)
     if this['event'] != 'updated':
         continue
-    title = '%s: updated from %s to %s' % (this['name'], this['old_version'], this['version'])
+    title = '%s: updated from %s to %s' % (
+        this['name'], this['old_version'], this['version'])
     opened = False
     for issue in open_issues:
         if issue.title.startswith(this['name'] + ': '):
             opened = True
             if issue.title != title:
-                issue.edit(title=title)
+                editable_issue = repo.issues.get(issue.iid, lazy=True)
+                editable_issue.title = title
+                editable_issue.save()
             break
     if not opened:
-        repo.create_issue(title=title, labels=['out-of-date'], assignee='imlonghao')
+        repo.issues.create({
+            'title': title,
+            'labels': 'out-of-date',
+            'assignee_ids': [535131]
+        })

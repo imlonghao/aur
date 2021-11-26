e52c22bc3491f54d3970d85122cd6cd1e54590b3
imlonghao <git@imlonghao.com>
2021-11-26 20:24:33 +0800

    CI: migrate to drone

--- nvchecker.py
+++ nvchecker.py
@@ -3,32 +3,45 @@
 import os
 import sys
 import json
-from gitlab import Gitlab
+import giteapy
 
-g = Gitlab('https://gitlab.com', private_token=os.environ['GITLAB_TOKEN'])
-repo = g.projects.get(19694297)
-open_issues = repo.issues.list(state='opened', labels=['out-of-date'])
+owner = "imlonghao"
+repo = "aur"
+
+configuration = giteapy.Configuration()
+configuration.host = "https://git.esd.cc/api/v1"
+configuration.api_key["token"] = os.environ["GITEA_BOT_TOKEN"]
+api_instance = giteapy.IssueApi(giteapy.ApiClient(configuration))
+open_issues = api_instance.issue_list_issues(
+    owner, repo, state="open", labels="out-of-date", page=-1
+)
 
 for line in sys.stdin:
     this = json.loads(line)
     print(this)
-    if this['event'] != 'updated':
+    if this["event"] != "updated":
         continue
-    title = '%s: updated from %s to %s' % (
-        this['name'], this['old_version'], this['version'])
+    title = "%s: updated from %s to %s" % (
+        this["name"],
+        this["old_version"],
+        this["version"],
+    )
     opened = False
     for issue in open_issues:
-        if issue.title.startswith(this['name'] + ': '):
+        if issue.title.startswith(this["name"] + ": "):
             opened = True
             if issue.title != title:
-                editable_issue = repo.issues.get(issue.iid, lazy=True)
-                editable_issue.title = title
-                editable_issue.save()
+                api_instance.issue_edit_issue(
+                    owner, repo, issue.id, body={"title": title}
+                )
             break
     if not opened:
-        repo.issues.create({
-            'title': title,
-            'labels': 'out-of-date',
-            'description': f'The package `{this["name"]}` is out of date,  `{this["old_version"]}` -> `{this["version"]}`\nplease update it to the latest version ASAP.\n\nHave a good day. :)',
-            'assignee_ids': [535131]
-        })
+        api_instance.api_instance(
+            owner,
+            repo,
+            body={
+                "title": title,
+                "assignee": "imlonghao",
+                "labels": [1],
+            },
+        )

ca88090a4e0de326ccc3808725180019dc593299
imlonghao <git@imlonghao.com>
2023-12-16 17:38:04 +0800

    ci: update .SRCINFO generatation

--- sync.sh
+++ sync.sh
@@ -8,16 +8,14 @@ DIRECTORY=$(pwd)
 git config --global user.email "aur@esd.cc"
 git config --global user.name "imlonghao"
 
-git log -1 --pretty=%B > /tmp/commit
+git log -1 --pretty=%B >/tmp/commit
 
-for REPO in $REPOS
-do
+for REPO in $REPOS; do
     cd /tmp
     git clone ssh://aur@aur.archlinux.org/$REPO.git
     chmod 777 $REPO
     cd $REPO
     cp -r $DIRECTORY/$REPO/. .
-    sudo -u nobody makepkg --pr > .SRCINFO
     git add .
     git commit -m "$(fgrep "$REPO: " /tmp/commit | awk -F "$REPO: " '{print $2}')"
     git push

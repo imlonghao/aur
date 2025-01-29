2019e2dcd0e1ef4c2e568bfeead8756abe7ebf59
imlonghao <git@imlonghao.com>
2025-01-29 10:23:53 +0800

    ci: fix error

--- sync.sh
+++ sync.sh
@@ -11,12 +11,16 @@ git config --global user.name "imlonghao"
 git log -1 --pretty=%B >/tmp/commit
 
 for REPO in $REPOS; do
+    commit_msg=$(fgrep "$REPO: " /tmp/commit | awk -F "$REPO: " '{print $2}')
+    if [ -z "$commit_msg" ] || [[ "$commit_msg" == *"SKIP"* ]]; then
+        continue
+    fi
     cd /tmp
     git clone ssh://aur@aur.archlinux.org/$REPO.git
     chmod 777 $REPO
     cd $REPO
     cp -r $DIRECTORY/$REPO/. .
     git add .
-    git commit -m "$(fgrep "$REPO: " /tmp/commit | awk -F "$REPO: " '{print $2}')"
+    git commit -m "$commit_msg"
     git push
 done
